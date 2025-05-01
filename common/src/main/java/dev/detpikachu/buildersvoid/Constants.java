5fb9f6b49b064c5e3e12aa18d045d06f02d66223
Andrei Hava <me@detpikachu.dev>
2025-05-01 19:47:12 +0300

    initial commit

---
+++ common/src/main/java/dev/detpikachu/buildersvoid/Constants.java
@@ -0,0 +1,10 @@
+package dev.detpikachu.buildersvoid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "buildersvoid";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_ID);
+}
