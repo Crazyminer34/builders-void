0df46ff0f3683b03f49d1677e2ce4e25f1805ddb
Andrei Hava <me@detpikachu.dev>
2025-05-02 10:18:33 +0300

    add datagen, yeetus experimentus

--- common/src/main/java/dev/detpikachu/buildersvoid/item/VoidPearl.java
+++ common/src/main/java/dev/detpikachu/buildersvoid/item/VoidPearl.java
@@ -1,6 +1,12 @@
 package dev.detpikachu.buildersvoid.item;
 
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 public class VoidPearl extends Item {
 
@@ -10,5 +16,15 @@ public class VoidPearl extends Item {
             .stacksTo(1)
             .fireResistant()
         );
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+        if (level.isClientSide) {
+            return InteractionResultHolder.pass(player.getItemInHand(usedHand));
+        }
+
+        player.sendSystemMessage(Component.literal("Hello!"));
+        return InteractionResultHolder.pass(player.getItemInHand(usedHand));
     }
 }
